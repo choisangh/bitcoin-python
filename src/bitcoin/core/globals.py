4269b9794c5e5cec50c2316fc980dd4ccd16b404
SH.CHOI <92343445+choisangh@users.noreply.github.com>
2023-03-10 22:12:12 +0900

    FEAT: 전역변수 추가

---
+++ src/bitcoin/core/globals.py
@@ -0,0 +1,6 @@
+from .transaction_pool import TransactionPool
+from .walletmanager import WalletManager
+
+TRANSACTION_POOL = TransactionPool()
+WALLET_MANAGER = WalletManager()
+DIFFICULTY_TARGET = 4
