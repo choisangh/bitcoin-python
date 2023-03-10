e379bd5b7d68b1d94332b5ac7a1ccb4a746da3c8
SH.CHOI <92343445+choisangh@users.noreply.github.com>
2023-03-10 22:13:08 +0900

    FEAT : main 함수 변경사항 반영

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from src.bitcoin.core import Blockchain
-from src.bitcoin.core import Wallet
+from src.bitcoin.core import WALLET_MANAGER
 from src.bitcoin.core import Miner
 
 
+
 if __name__ == '__main__':
-    # Alice's Wallet
-    alice_wallet = Wallet()
-    alice_address = alice_wallet.public_key.to_string().hex()
-    print("Alice's Address: ", alice_address)
+
 
     # Bob's Wallet
-    bob_wallet = Wallet()
+    bob_wallet = WALLET_MANAGER.create_wallet()
     bob_address = bob_wallet.public_key.to_string().hex()
     print("Bob's Address: ", bob_address)
 
-    # Alice sends transaction to Bob
-    transaction, signature = alice_wallet.send_transaction(bob_address, 5)
-    print("Transaction Hash: ", transaction.calculate_hash())
-    print("Transaction Signature: ", signature.hex())
-
     # 비트코인 노드 초기화
     blockchain = Blockchain()
-    wallet = Wallet()
-    miner = Miner(blockchain, wallet)
-    print(wallet.get_public_key())
+    miner = Miner(blockchain, bob_wallet)
     # 채굴 시작
     print("채굴을 시작합니다...")
-    while True:
+    for i in range(10):
         block = miner.mine()
-        print(f"새로운 블록 채굴 완료: {block}")
+        print(f"새로운 블록 채굴 완료: {block.to_dict()}")
+    print(bob_wallet.balance)
+    # Alice's Wallet
+    alice_wallet = WALLET_MANAGER.create_wallet()
+    alice_address = alice_wallet.public_key.to_string().hex()
+    print("Alice's Address: ", alice_address)
+    # Alice sends transaction to Bob
+    transaction, signature = bob_wallet.send_transaction(alice_address, 5)
+    print("Transaction Hash: ", transaction.calculate_hash())
+    print("Transaction Signature: ", signature)
+    print(alice_address)
+
+    print("Bob's wallet balance: ", bob_wallet.balance)
+    print("Alice's wallet balance: ", alice_wallet.balance)
+
+
 
